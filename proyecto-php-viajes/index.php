701a8494c8313e83c0a23cbb0587e0027cbfb3f5
aibanezm-esliceu <aibanezm@esliceu.net>
2026-05-09 11:53:32 +0200

    creación de php's y inserción en el index.php

---
+++ proyecto-php-viajes/index.php
@@ -0,0 +1,17 @@
+<?php 
+// 1. Definimos el título de esta página en concreto
+$pageTitle = "Inicio - Tu agencia de viajes de confianza"; 
+
+// 2. Traemos todo el código de arriba (HTML, CSS y Menú)
+require __DIR__ . "/includes/header.php"; 
+?>
+
+<section>
+    <h1>¡Bienvenido viajero!</h1>
+    <p>Este es el proyecto de examen. El header y el footer se están cargando correctamente usando PHP.</p>
+</section>
+
+<?php 
+// 4. Traemos todo el código de abajo (cierre de etiquetas)
+require __DIR__ . "/includes/footer.php"; 
+?>
